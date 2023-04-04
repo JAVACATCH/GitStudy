b62a0eea594be5077cf6c9000422c5daa7ae9ddc
kjlove0226 <128684772+kjlove0226@users.noreply.github.com>
2023-04-05 03:59:27 +0900

    4.6
    숙제

---
+++ minsung/division.java
@@ -0,0 +1,13 @@
+
+public class division {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 2;
+		int c = 0;
+		
+		c = a / b;
+		System.out.println(c);
+	}
+
+}
---
+++ minsung/plus.java
@@ -0,0 +1,15 @@
+
+public class plus {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 20;
+		int c = 0;
+		
+		c = a + b;
+		System.out.println(c);
+		
+
+	}
+
+}
